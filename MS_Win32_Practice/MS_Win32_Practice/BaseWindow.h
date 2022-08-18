e6dbf9e19536290c4f1b8965a79b6f287b135c2a
Joons <elddy0948@gmail.com>
2022-08-18 10:49:50 +0900

    practice ms win32 application (have to fix error)

---
+++ MS_Win32_Practice/MS_Win32_Practice/BaseWindow.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace base 
+{
+	template <class DERIVED_TYPE>
+	class BaseWindow
+	{
+	public:
+		static LRESULT CALLBACK WindowProc(
+			HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+		BaseWindow();
+		BOOL Create(
+			PCWSTR lpWindowName,
+			DWORD dwStyle,
+			DWORD dwExStyle = 0,
+			int x = CW_USEDEFAULT, int y = CW_USEDEFAULT,
+			int nWidth = CW_USEDEFAULT, int nHeight = CW_USEDEFAULT,
+			HWND hWndParent = 0,
+			HMENU hMenu = 0);
+
+		HWND Window() const { return m_hwnd; };
+	protected:
+		virtual PCWSTR ClassName() const = 0;
+		virtual LRESULT HandleMessage(
+			UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+		HWND m_hwnd;
+	};
+}
---
+++ MS_Win32_Practice/MS_Win32_Practice/MainWindow.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "BaseWindow.h"
+
+namespace base
+{
+	class MainWindow : public BaseWindow<MainWindow>
+	{
+	public:
+		PCWSTR ClassName() const { return L"Sample Window Class"; }
+		LRESULT HandleMessage(
+			UINT uMsg, WPARAM wParam, LPARAM lParam);
+	};
+}
