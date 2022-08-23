8406ce0876387ae91da98ffde553319fae1799b3
Joons <elddy0948@gmail.com>
2022-08-23 21:00:05 +0900

    Create project JoonsEngine
    Implementation ShowWindow. but not something with message

---
+++ JoonsEngine/JoonsEngine/WinMain.cpp
@@ -0,0 +1,42 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd
+)
+{
+
+	//Create Windows
+	WNDCLASSEX wc{ 0 };
+
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = DLGWINDOWEXTRA;
+	wc.hInstance = hInstance;
+	wc.hCursor = NULL;
+	wc.hbrBackground = HBRUSH(COLOR_WINDOW);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = L"JoonsEngine";
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	// Windows Instance
+	HWND hWnd;
+
+	hWnd = CreateWindowExA(
+		0, "JoonsEngine",
+		"Joons Engine Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_CAPTION,
+		200, 200,
+		600, 400,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (true);
+}
