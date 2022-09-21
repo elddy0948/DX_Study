76157b240775ed824358f065c6faf59c659d8b9a
Hojoon Kim <elddy0948@gmail.com>
2022-09-22 02:16:16 +0900

    class template practice

---
+++ SomePracticalCode/array_template/Point.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+  int xpos, ypos;
+
+public:
+  Point(int x = 0, int y = 0);
+  friend ostream &operator<<(ostream &os, const Point &pos);
+};
