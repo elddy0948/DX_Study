50b23f2171ae57b4b05218d64af12f6bdf4f45d2
Hojoon Kim <elddy0948@gmail.com>
2022-11-01 22:54:01 +0900

    Implementation InitWindowsApp

--- Windows_Programming_Basic/Windows_Programming_Basic/Windows_Programming_Basic.cpp
+++ Windows_Programming_Basic/Windows_Programming_Basic/Windows_Programming_Basic.cpp
@@ -15,3 +15,49 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
 
 	return Run();
 }
+
+bool InitWindowsApp(HINSTANCE instanceHandle, int show)
+{
+	WNDCLASS wc;
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = instanceHandle;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"BasicWndClass";
+
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"RegisterClass FAILED", 0, 0);
+		return false;
+	}
+
+	ghMainWnd = CreateWindow(
+		L"BasicWndClass",
+		L"Win32Basic",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		instanceHandle,
+		0);
+
+	if (ghMainWnd == 0)
+	{
+		MessageBox(0, L"CreateWindow FAILED", 0, 0);
+		return false;
+	}
+
+	ShowWindow(ghMainWnd, show);
+	UpdateWindow(ghMainWnd);
+
+	return true;
+}
