176ffc635bf1703c95d58afb6e2540d60cebd406
Hojoon Kim <elddy0948@gmail.com>
2022-11-07 15:51:16 +0900

    create InitD3D project

---
+++ InitD3D/InitD3D/InitD3D.cpp
@@ -0,0 +1,93 @@
+#include "framework.h"
+#include "InitD3D.h"
+
+HWND ghMainWnd = 0;
+
+bool InitWindowsApp(HINSTANCE instanceHandle, int show);
+int Run();
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nShowCmd)
+{
+	if (!InitWindowsApp(hInstance, nShowCmd)) { return 0; }
+
+	return Run();
+}
+
+bool InitWindowsApp(HINSTANCE instanceHandle, int show)
+{
+	WNDCLASS wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = instanceHandle;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"BasicWindowsClass";
+
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"RegisterClass Failed", 0, 0);
+		return false;
+	}
+
+	ghMainWnd = CreateWindow(
+		L"BasicWindowsClass",
+		L"InitD3d",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		instanceHandle,
+		0
+	);
+
+	if (ghMainWnd == 0)
+	{
+		MessageBox(0, L"CreateWindow Failed", 0, 0);
+		return false;
+	}
+
+	ShowWindow(ghMainWnd, show);
+	UpdateWindow(ghMainWnd);
+}
+
+int Run()
+{
+	MSG msg = { 0 };
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) { DestroyWindow(ghMainWnd); }
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
