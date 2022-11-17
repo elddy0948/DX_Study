6366eeb84b4cc72b0a2c3c0f116ef77af5d5dda5
Hojoon Kim <elddy0948@gmail.com>
2022-11-17 16:09:50 +0900

    [Feat] GameTimer some feat implementation
    have to implement Start(), GameTime()

---
+++ InitD3D/InitD3D/GameTimer.cpp
@@ -0,0 +1,66 @@
+#include "GameTimer.h"
+
+GameTimer::GameTimer()
+	: mSecondsPerCount(0.0), mDeltaTime(-1.0), mBaseTime(0),
+	mPausedTime(0), mStopTime(0), mPrevTime(0), mCurrTime(0),
+	mStopped(false)
+{
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+	mSecondsPerCount = 1.0 / (double)countsPerSec;
+}
+
+float GameTimer::GameTime() const
+{
+
+}
+
+float GameTimer::DeltaTime() const
+{
+	return (float)mDeltaTime;
+}
+
+void GameTimer::Reset()
+{
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+
+	mBaseTime = currTime;
+	mPrevTime = currTime;
+	mStopTime = 0;
+	mStopped = false;
+}
+
+void GameTimer::Start()
+{
+
+}
+
+void GameTimer::Stop()
+{
+	if (!mStopped)
+	{
+		__int64 currTime;
+		QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+		mStopTime = currTime;
+		mStopped = true;
+	}
+}
+
+void GameTimer::Tick()
+{
+	if (mStopped)
+	{
+		mDeltaTime = 0.0;
+		return;
+	}
+
+	__int64 currTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currTime);
+	mCurrTime = currTime;
+
+	mDeltaTime = (mCurrTime - mPrevTime) * mSecondsPerCount;
+	mPrevTime = mCurrTime;
+
+	if (mDeltaTime < 0.0) { mDeltaTime = 0.0; }
+}
