c2b0d918e7585e72fdfaaa303dcf9a89d2f2ffb8
Hojoon Kim <elddy0948@gmail.com>
2023-08-20 23:09:30 +0900

    fix some code in geometry generator

--- NotOnlyCube/NotOnlyCube/D3DHelper.h
+++ NotOnlyCube/NotOnlyCube/D3DHelper.h
@@ -108,61 +108,6 @@ inline std::wstring AnsiToWString(const std::string& str)
 	return std::wstring(buffer);
 }
 
-struct SubmeshGeometry
-{
-	UINT IndexCount = 0;
-	UINT StartIndexLocation = 0;
-	INT BaseVertexLocation = 0;
-
-	DirectX::BoundingBox Bounds;
-};
-
-struct MeshGeometry
-{
-	std::string Name;
-
-	Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
-	Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
-
-	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
-	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
-
-	Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
-	Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
-
-
-	UINT VertexByteStride = 0;
-	UINT VertexBufferByteSize = 0;
-	DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
-	UINT indexBufferByteSize = 0;
-
-	std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
-
-	D3D12_VERTEX_BUFFER_VIEW VertexBufferView() const
-	{
-		D3D12_VERTEX_BUFFER_VIEW vbv = {};
-		vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
-		vbv.StrideInBytes = VertexByteStride;
-		vbv.SizeInBytes = VertexBufferByteSize;
-		return vbv;
-	}
-
-	D3D12_INDEX_BUFFER_VIEW IndexBufferView() const
-	{
-		D3D12_INDEX_BUFFER_VIEW ibv = {};
-		ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
-		ibv.Format = IndexFormat;
-		ibv.SizeInBytes = indexBufferByteSize;
-		return ibv;
-	}
-
-	void DisposeUploaders()
-	{
-		VertexBufferUploader = nullptr;
-		IndexBufferUploader = nullptr;
-	}
-};
-
 #ifndef ThrowIfFailed
 #define ThrowIfFailed(x) \
 { \
--- NotOnlyCube/NotOnlyCube/GeometryGenerator.cpp
+++ NotOnlyCube/NotOnlyCube/GeometryGenerator.cpp
@@ -67,75 +67,6 @@ GeometryGenerator::CreateCylinder(
 	return meshData;
 }
 
-void GeometryGenerator::BuildCylinderTopCap(
-	float bottomRadius,
-	float topRadius,
-	float height,
-	uint32 sliceCount,
-	uint32 stackCount,
-	GeometryGenerator::MeshData& meshData
-)
-{
-  uint32 baseIndex = (uint32)meshData.Vertices.size();
-  float y = 0.5f * height;
-  float dTheta = 2.0f * DirectX::XM_PI / sliceCount;
-
-  for (uint32 i = 0; i <= sliceCount; ++i)
-  {
-    float x = topRadius * cosf(i * dTheta);
-    float z = topRadius * sinf(i * dTheta);
-
-    float u = x / height + 0.5f;
-    float v = z / height + 0.5f;
-
-    meshData.Vertices.push_back(Vertex(x, y, z, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, u, v));
-  }
-
-  meshData.Vertices.push_back(Vertex(0.0f, y, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f));
-
-  uint32 centerIndex = (uint32)meshData.Vertices.size() - 1;
-
-  for (uint32 i = 0; i < sliceCount; ++i)
-  {
-    meshData.Indices32.push_back(centerIndex);
-    meshData.Indices32.push_back(baseIndex + i + 1);
-    meshData.Indices32.push_back(baseIndex + i);
-  }
-}
-
-void GeometryGenerator::BuildCylinderBottomCap(
-	float bottomRadius,
-	float topRadius,
-	float height,
-	uint32 sliceCount,
-	uint32 stackCount,
-	GeometryGenerator::MeshData& meshData
-)
-{
-  uint32 baseIndex = (uint32)meshData.Vertices.size();
-  float y = -0.5f * height;
-  float dTheta = 2.0f * DirectX::XM_PI / sliceCount;
-
-  for (uint32 i = 0; i <= sliceCount; ++i)
-  {
-    float x = bottomRadius * cosf(i * dTheta);
-    float z = bottomRadius * sinf(i * dTheta);
-
-    float u = x / height + 0.5f;
-    float v = z / height + 0.5f;
-
-    meshData.Vertices.push_back(Vertex(x, y, z, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, u, v));
-  }
-  meshData.Vertices.push_back(Vertex(0.0f, y, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f));
-  uint32 centerIndex = (uint32)meshData.Vertices.size() - 1;
-  for (uint32 i = 0; i < sliceCount; ++i)
-  {
-    meshData.Indices32.push_back(centerIndex);
-    meshData.Indices32.push_back(baseIndex + i);
-    meshData.Indices32.push_back(baseIndex + i + 1);
-  }
-}
-
 GeometryGenerator::MeshData GeometryGenerator::CreateSphere(
   float radius,
   uint32 sliceCount,
@@ -226,39 +157,186 @@ GeometryGenerator::MeshData GeometryGenerator::CreateGeoSphere(
   uint32 numSubdivisioins
 )
 {
-  MeshData meshData;
+    using namespace DirectX;
 
-  numSubdivisioins = std::min<uint32>(numSubdivisioins, 6u);
+    MeshData meshData;
 
-  const float X = 0.525731f;
-  const float Z = 0.850651f;
+    numSubdivisioins = std::min<uint32>(numSubdivisioins, 6u);
 
-  DirectX::XMFLOAT3 pos[12] = 
-  {
-    DirectX::XMFLOAT3(-X, 0.0f, Z), DirectX::XMFLOAT3(X, 0.0f, Z),
-    DirectX::XMFLOAT3(-X, 0.0f, -Z), DirectX::XMFLOAT3(X, 0.0f, -Z),
-    DirectX::XMFLOAT3(0.0f, Z, X), DirectX::XMFLOAT3(0.0f, Z, -X),
-    DirectX::XMFLOAT3(0.0f, -Z, X), DirectX::XMFLOAT3(0.0f, -Z, -X),
-    DirectX::XMFLOAT3(Z, X, 0.0f), DirectX::XMFLOAT3(-Z, X, 0.0f),
-    DirectX::XMFLOAT3(Z, -X, 0.0f), DirectX::XMFLOAT3(-Z, -X, 0.0f)
-  };
+    const float X = 0.525731f;
+    const float Z = 0.850651f;
 
-  uint32 k[60] =
-  {
-      1, 4, 0,  4, 9, 0,  4, 5, 9,  8, 5, 4,  1, 8, 4,
-      1, 10, 8,  10, 3, 8,  8, 3, 5,  3, 2, 5,  3, 7, 2,
-      3, 10, 7,  10, 6, 7,  6, 11, 7,  6, 0, 11,  6, 1, 0,
-      10, 1, 6,  11, 0, 9,  2, 11, 9,  5, 2, 9,  11, 2, 7
-  };
+    XMFLOAT3 pos[12] = 
+    {
+      XMFLOAT3(-X, 0.0f, Z), XMFLOAT3(X, 0.0f, Z),
+      XMFLOAT3(-X, 0.0f, -Z), XMFLOAT3(X, 0.0f, -Z),
+      XMFLOAT3(0.0f, Z, X), XMFLOAT3(0.0f, Z, -X),
+      XMFLOAT3(0.0f, -Z, X), XMFLOAT3(0.0f, -Z, -X),
+      XMFLOAT3(Z, X, 0.0f), XMFLOAT3(-Z, X, 0.0f),
+      XMFLOAT3(Z, -X, 0.0f), XMFLOAT3(-Z, -X, 0.0f)
+    };
 
-  meshData.Vertices.resize(12);
-  meshData.Indices32.assign(&k[0], &k[60]);
+    uint32 k[60] =
+    {
+        1, 4, 0,  4, 9, 0,  4, 5, 9,  8, 5, 4,  1, 8, 4,
+        1, 10, 8,  10, 3, 8,  8, 3, 5,  3, 2, 5,  3, 7, 2,
+        3, 10, 7,  10, 6, 7,  6, 11, 7,  6, 0, 11,  6, 1, 0,
+        10, 1, 6,  11, 0, 9,  2, 11, 9,  5, 2, 9,  11, 2, 7
+    };
 
-  for (uint32 i = 0; i < 12; ++i)
-    meshData.Vertices[i].Position = pos[i];
+    meshData.Vertices.resize(12);
+    meshData.Indices32.assign(&k[0], &k[60]);
 
-  for (uint32 i = 0; i < numSubdivisioins; ++i)
-    Subdivide(meshData);
+    for (uint32 i = 0; i < 12; ++i)
+      meshData.Vertices[i].Position = pos[i];
+
+    for (uint32 i = 0; i < numSubdivisioins; ++i)
+      Subdivide(meshData);
+
+    for (uint32 i = 0; i < meshData.Vertices.size(); ++i)
+    {
+        XMVECTOR n = XMVector3Normalize(
+            XMLoadFloat3(&meshData.Vertices[i].Position)
+        );
+
+        XMVECTOR p = radius * n;
+
+        XMStoreFloat3(&meshData.Vertices[i].Position, p);
+        XMStoreFloat3(&meshData.Vertices[i].Normal, n);
+
+        float theta = atan2f(
+            meshData.Vertices[i].Position.z,
+            meshData.Vertices[i].Position.x
+        );
+
+        if (theta < 0.0f)
+            theta += XM_2PI;
+
+        float phi = acosf(meshData.Vertices[i].Position.y / radius);
+
+        meshData.Vertices[i].TexC.x = theta / XM_2PI;
+        meshData.Vertices[i].TexC.y = phi / XM_PI;
+
+        meshData.Vertices[i].TangentU.x = -radius * sinf(phi) * sinf(theta);
+        meshData.Vertices[i].TangentU.y = 0.0f;
+        meshData.Vertices[i].TangentU.z = radius * sinf(phi) * cosf(theta);
+
+        XMVECTOR T = XMLoadFloat3(&meshData.Vertices[i].TangentU);
+        XMStoreFloat3(&meshData.Vertices[i].TangentU, XMVector3Normalize(T));
+    }
+    return meshData;
+}
+
+void GeometryGenerator::Subdivide(MeshData& meshData)
+{
+    MeshData inputCopy = meshData;
+
+    meshData.Vertices.resize(0);
+    meshData.Indices32.resize(0);
+
+    uint32 numTris = (uint32)inputCopy.Indices32.size() / 3;
+    for (uint32 i = 0; i < numTris; ++i)
+    {
+        Vertex v0 = inputCopy.Vertices[inputCopy.Indices32[i * 3 + 0]];
+        Vertex v1 = inputCopy.Vertices[inputCopy.Indices32[i * 3 + 1]];
+        Vertex v2 = inputCopy.Vertices[inputCopy.Indices32[i * 3 + 2]];
+
+        Vertex m0 = MidPoint(v0, v1);
+        Vertex m1 = MidPoint(v1, v2);
+        Vertex m2 = MidPoint(v0, v2);
+
+        meshData.Vertices.push_back(v0);
+        meshData.Vertices.push_back(v1);
+        meshData.Vertices.push_back(v2);
+        meshData.Vertices.push_back(m0);
+        meshData.Vertices.push_back(m1);
+        meshData.Vertices.push_back(m2);
+
+        meshData.Indices32.push_back(i * 6 + 0);
+        meshData.Indices32.push_back(i * 6 + 3);
+        meshData.Indices32.push_back(i * 6 + 5);
+
+        meshData.Indices32.push_back(i * 6 + 3);
+        meshData.Indices32.push_back(i * 6 + 4);
+        meshData.Indices32.push_back(i * 6 + 5);
+
+        meshData.Indices32.push_back(i * 6 + 5);
+        meshData.Indices32.push_back(i * 6 + 4);
+        meshData.Indices32.push_back(i * 6 + 2);
+
+        meshData.Indices32.push_back(i * 6 + 3);
+        meshData.Indices32.push_back(i * 6 + 1);
+        meshData.Indices32.push_back(i * 6 + 4);
+    }
+}
+
+void GeometryGenerator::BuildCylinderTopCap(
+    float bottomRadius,
+    float topRadius,
+    float height,
+    uint32 sliceCount,
+    uint32 stackCount,
+    GeometryGenerator::MeshData& meshData
+)
+{
+    uint32 baseIndex = (uint32)meshData.Vertices.size();
+    float y = 0.5f * height;
+    float dTheta = 2.0f * DirectX::XM_PI / sliceCount;
+
+    for (uint32 i = 0; i <= sliceCount; ++i)
+    {
+        float x = topRadius * cosf(i * dTheta);
+        float z = topRadius * sinf(i * dTheta);
+
+        float u = x / height + 0.5f;
+        float v = z / height + 0.5f;
+
+        meshData.Vertices.push_back(Vertex(x, y, z, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, u, v));
+    }
+
+    meshData.Vertices.push_back(Vertex(0.0f, y, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f));
+
+    uint32 centerIndex = (uint32)meshData.Vertices.size() - 1;
+
+    for (uint32 i = 0; i < sliceCount; ++i)
+    {
+        meshData.Indices32.push_back(centerIndex);
+        meshData.Indices32.push_back(baseIndex + i + 1);
+        meshData.Indices32.push_back(baseIndex + i);
+    }
+}
+
+void GeometryGenerator::BuildCylinderBottomCap(
+    float bottomRadius,
+    float topRadius,
+    float height,
+    uint32 sliceCount,
+    uint32 stackCount,
+    GeometryGenerator::MeshData& meshData
+)
+{
+    uint32 baseIndex = (uint32)meshData.Vertices.size();
+    float y = -0.5f * height;
+    float dTheta = 2.0f * DirectX::XM_PI / sliceCount;
+
+    for (uint32 i = 0; i <= sliceCount; ++i)
+    {
+        float x = bottomRadius * cosf(i * dTheta);
+        float z = bottomRadius * sinf(i * dTheta);
+
+        float u = x / height + 0.5f;
+        float v = z / height + 0.5f;
+
+        meshData.Vertices.push_back(Vertex(x, y, z, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, u, v));
+    }
+    meshData.Vertices.push_back(Vertex(0.0f, y, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 0.5f));
+    uint32 centerIndex = (uint32)meshData.Vertices.size() - 1;
+    for (uint32 i = 0; i < sliceCount; ++i)
+    {
+        meshData.Indices32.push_back(centerIndex);
+        meshData.Indices32.push_back(baseIndex + i);
+        meshData.Indices32.push_back(baseIndex + i + 1);
+    }
 }
 
 GeometryGenerator::Vertex GeometryGenerator::MidPoint(const Vertex& v0, const Vertex& v1)
@@ -271,6 +349,22 @@ GeometryGenerator::Vertex GeometryGenerator::MidPoint(const Vertex& v0, const Ve
     XMVECTOR n0 = XMLoadFloat3(&v0.Normal);
     XMVECTOR n1 = XMLoadFloat3(&v1.Normal);
 
-    XMVECTOR t0 = XMLoadFloat3(&v0.TangentU);
-    XMVECTOR t1 = XMLoadFloat3(&v1.TangentU);
+    XMVECTOR tan0 = XMLoadFloat3(&v0.TangentU);
+    XMVECTOR tan1 = XMLoadFloat3(&v1.TangentU);
+
+    XMVECTOR tex0 = XMLoadFloat2(&v0.TexC);
+    XMVECTOR tex1 = XMLoadFloat2(&v1.TexC);
+
+    XMVECTOR pos = 0.5f * (p0 + p1);
+    XMVECTOR normal = XMVector3Normalize(0.5f * (n0 + n1));
+    XMVECTOR tangent = XMVector3Normalize(0.5f * (tan0 + tan1));
+    XMVECTOR tex = 0.5f * (tex0 + tex1);
+
+    Vertex v;
+    XMStoreFloat3(&v.Position, pos);
+    XMStoreFloat3(&v.Normal, normal);
+    XMStoreFloat3(&v.TangentU, tangent);
+    XMStoreFloat2(&v.TexC, tex);
+
+    return v;
 }
