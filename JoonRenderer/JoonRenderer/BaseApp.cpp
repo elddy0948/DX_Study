4b6785643ac71911ef52ed63e8d1d2d27addbfb7
Hojoon Kim <elddy0948@gmail.com>
2023-08-30 00:01:43 +0900

    feat : LogOutputDisplayModes

--- JoonRenderer/JoonRenderer/BaseApp.cpp
+++ JoonRenderer/JoonRenderer/BaseApp.cpp
@@ -42,10 +42,51 @@ void BaseApp::LogAdapters()
 
 void BaseApp::LogAdapterOutputs(IDXGIAdapter* adapter)
 {
+	UINT i = 0;
+	IDXGIOutput* output = nullptr;
+	while (adapter->EnumOutputs(i, &output) != DXGI_ERROR_NOT_FOUND)
+	{
+		DXGI_OUTPUT_DESC outputDesc;
+		output->GetDesc(&outputDesc);
 
+		std::wstring text = L"*** Output: ";
+		text += outputDesc.DeviceName;
+		text += L"\n";
+
+		OutputDebugString(text.c_str());
+
+		LogOutputDisplayModes(output, DXGI_FORMAT_B8G8R8A8_UNORM);
+
+		if (output)
+		{
+			output->Release();
+			output = 0;
+		}
+
+		++i;
+	}
 }
 
 void BaseApp::LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format)
 {
+	UINT count = 0;
+	UINT flags = 0;
 
+	output->GetDisplayModeList(format, flags, &count, nullptr);
+
+	std::vector<DXGI_MODE_DESC> modeList(count);
+	output->GetDisplayModeList(format, flags, &count, &modeList[0]);
+
+	for (auto& mode : modeList)
+	{
+		UINT n = mode.RefreshRate.Numerator;
+		UINT d = mode.RefreshRate.Denominator;
+
+		std::wstring text =
+			L"Width = " + std::to_wstring(mode.Width) + L" " +
+			L"Height = " + std::to_wstring(mode.Height) + L" " +
+			L"Refresh = " + std::to_wstring(n) + L"/" + std::to_wstring(d) + L"\n";
+
+		::OutputDebugString(text.c_str());
+	}
 }
