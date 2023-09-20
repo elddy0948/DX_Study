b20123c3d77a22c3531f35f02c5cdd54381098ce
Hojoon Kim <elddy0948@gmail.com>
2023-09-20 16:04:38 +0900

    feat : Create FrameResource for LitWavesApp

--- JoonRenderer/JoonRenderer/LitWavesApp.cpp
+++ JoonRenderer/JoonRenderer/LitWavesApp.cpp
@@ -1,5 +1,18 @@
 #include "LitWavesApp.h"
 
+// FrameResource
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT wavesVertexCount, UINT materialCount)
+{
+	ThrowIfFailed(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(CommandAllocator.GetAddressOf())));
+	ObjectConstantsBuffer = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+	PassConstantsBuffer = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+	VertexBuffer = std::make_unique<UploadBuffer<Vertex>>(device, wavesVertexCount, false);
+	MaterialConstantsBuffer = std::make_unique<UploadBuffer<MaterialConstants>>(device, materialCount, true);
+}
+
+FrameResource::~FrameResource() {}
+
+// LitWavesApp
 LitWavesApp::LitWavesApp(HINSTANCE hInstance) : BaseApp(hInstance) {}
 
 LitWavesApp::~LitWavesApp()
--- JoonRenderer/JoonRenderer/LitWavesApp.h
+++ JoonRenderer/JoonRenderer/LitWavesApp.h
@@ -4,6 +4,49 @@
 #include "BaseApp.h"
 #include "JRMath.h"
 #include "Material.h"
+#include "UploadBuffer.h"
+
+struct ObjectConstants
+{
+
+};
+
+struct PassConstants
+{
+
+};
+
+struct Vertex
+{
+
+};
+
+struct MaterialConstants
+{
+	DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
+	DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
+	float Roughness = 0.25f;
+	DirectX::XMFLOAT4X4 MaterialTransform = JRMath::Identity4x4();
+};
+
+struct FrameResource
+{
+public:
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT wavesVertexCount, UINT materialCount);
+	FrameResource(const FrameResource& rhs) = delete;
+	FrameResource& operator=(const FrameResource& rhs) = delete;
+	~FrameResource();
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CommandAllocator;
+
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectConstantsBuffer = nullptr;
+	std::unique_ptr<UploadBuffer<PassConstants>> PassConstantsBuffer = nullptr;
+
+	std::unique_ptr<UploadBuffer<Vertex>> VertexBuffer = nullptr;
+	std::unique_ptr<UploadBuffer<MaterialConstants>> MaterialConstantsBuffer = nullptr;
+
+	UINT64 fence = 0;
+};
 
 class LitWavesApp : public BaseApp
 {
